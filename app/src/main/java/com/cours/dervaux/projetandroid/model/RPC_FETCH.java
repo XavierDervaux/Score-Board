9e5b6e78a67420898380c7b33ac13d51cafa298a
Xavier Dervaux <xavier.dervaux@outlook.be>
2017-01-11 22:32:50 +0100

    Realease Candidate

--- app/src/main/java/com/cours/dervaux/projetandroid/model/RPC_FETCH.java
+++ app/src/main/java/com/cours/dervaux/projetandroid/model/RPC_FETCH.java
@@ -2,7 +2,6 @@ package com.cours.dervaux.projetandroid.model;
 
 public class RPC_FETCH { //Here to easiky access the RPC in the asynchronous functions, easy to modify if need be.
     public static String SERVER_URL = "http://192.168.1.149/projects/ProjetAndroid/"; //Home
-    //public static String SERVER_URL = "http://10.52.29.138/projects/ProjetAndroid/";  //School
     public static String CONNECTION = SERVER_URL + "se_connecter.php";
     public static String REGISTER   = SERVER_URL + "creer_compte.php";
     public static String ADD_SCORE  = SERVER_URL + "ajouter_score.php";
